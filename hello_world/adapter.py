c1b0e0e2a281244959be899d67a5715f637fa2b5
Jasper Koehorst <jasper.koehorst@wur.nl>
2024-11-20 07:22:20 +0100

    Fixed some requirements for the default adapter to work

--- hello_world/adapter.py
+++ hello_world/adapter.py
@@ -1,7 +1,9 @@
 import logging
 import os
+from typing import Optional
 
 from leaf.adapters.equipment_adapter import EquipmentAdapter
+from leaf.error_handler.error_holder import ErrorHolder
 from leaf.metadata_manager.metadata import MetadataManager
 from leaf.modules.input_modules.polling_watcher import PollingWatcher
 from leaf.modules.input_modules.simple_watcher import SimpleWatcher
@@ -11,15 +13,19 @@ from leaf.modules.phase_modules.start import StartPhase
 from leaf.modules.phase_modules.stop import StopPhase
 from leaf.modules.process_modules.discrete_module import DiscreteProcess
 
-from interpreter import HelloWorldInterpreter
+from hello_world.interpreter import HelloWorldInterpreter
 
 logger = get_logger(__name__, log_file="app.log", log_level=logging.DEBUG)
 
 class HelloWorldAdapter(EquipmentAdapter):
     def __init__(
         self,
+        instance_data,
         output,
+        error_holder: Optional[ErrorHolder] = None,
+        interval: int = 10
     ) -> None:
+        self.error_holder = error_holder
         logger.info(
             f"Initializing DEMO Adapter"
         )
@@ -31,7 +37,7 @@ class HelloWorldAdapter(EquipmentAdapter):
         metadata_manager: MetadataManager = MetadataManager()
         metadata_manager.load_from_file(metadata_fn)
         # Create a polling watcher
-        watcher: PollingWatcher = SimpleWatcher(metadata_manager=metadata_manager, interval=10, measurement_callbacks=[])
+        watcher: PollingWatcher = SimpleWatcher(metadata_manager=metadata_manager, interval=interval, measurement_callbacks=[])
         # Create the phases?
         start_p: StartPhase = StartPhase(output, metadata_manager)
         stop_p: StopPhase = StopPhase(output, metadata_manager)
